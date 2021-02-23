47f28048337c66a42fa8cc00e0d604b09d45ec4c
Eduard Likwong <ejllikwong@gmail.com>
2021-02-23 23:31:44 +0800

    Added a separate header for Cards, added ellipsis icon for menu

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Grid } from './Grid';
 import { Stats } from './index';
+import { FaEllipsisH } from 'react-icons/fa';
 import './Card.css';
 
 function Card(props) {
@@ -16,11 +17,23 @@ function Card(props) {
 
   return (
     <div className="card">
-      <div className="card-header">
-        <span onClick={handleTitleClick}>{props.title}</span>
-      </div>
+      <Header title={props.title} handleTitleClick={handleTitleClick} />
       <Grid {...props} />
       <Stats card={card} />
+    </div>
+  );
+}
+
+function Header(props) {
+  const { title, handleTitleClick } = props;
+  return (
+    <div className="card-header">
+      <span onClick={handleTitleClick}>{title}</span>
+      <div className="card-menu">
+        <span>
+          <FaEllipsisH />
+        </span>
+      </div>
     </div>
   );
 }
